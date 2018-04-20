9fa425d404b4e62cfa33071be90de30147aca730
LiuPeng-PC-3 <2224214496@qq.com>
2018-04-20 23:35:27 +0800

    更换IDE为RStudio，从R basic入手.

--- #Code/01/01/Script.R
+++ #Code/01/01/Script.R
@@ -1,2 +1,6 @@
-data1 <- read.csv(file.choose(), header = T)
-data1[c(5, 6, 7, 8), ]
+read.csv('data 1.CSV', header = T) -> DATA
+
+cov(DATA[2], DATA[6])
+#cov(DATA[3], DATA[6])
+cov(DATA[4], DATA[6])
+#cov(DATA[5], DATA[6])
